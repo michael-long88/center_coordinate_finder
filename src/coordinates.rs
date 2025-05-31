b44f204d6880a631eb1013b5f8aa379bccfc7be6
Michael Long <michael.d.long88@gmail.com>
2025-05-30 21:52:50 -0400

    Change to structs

---
+++ src/coordinates.rs
@@ -0,0 +1,64 @@
+use std::{error::Error, f64::consts::PI, iter::zip};
+
+pub struct Coordinate {
+    latitude: f64,
+    longitude: f64,
+}
+
+impl Coordinate {
+    pub fn new(latitude: f64, longitude: f64) -> Coordinate {
+        Coordinate {
+            latitude,
+            longitude,
+        }
+    }
+
+    pub fn degrees_to_radians(&self) -> (f64, f64) {
+        (self.latitude * PI / 180.0, self.longitude * PI / 180.0)
+    }
+}
+
+pub fn coordinates_from_vectors(
+    latitudes: Vec<f64>,
+    longitudes: Vec<f64>,
+) -> Result<Vec<Coordinate>, Box<dyn Error>> {
+    if latitudes.len() != longitudes.len() {
+        return Err("Vectors of latitudes and longitudes must have the same length".into());
+    }
+
+    let coordinates: Vec<Coordinate> = zip(latitudes.iter(), longitudes.iter())
+        .map(|(lat, lon)| Coordinate::new(*lat, *lon))
+        .collect();
+
+    Ok(coordinates)
+}
+
+pub fn get_center_from_degrees(coordinates: Vec<Coordinate>) -> (f64, f64) {
+    let mut x: f64 = 0.0;
+    let mut y: f64 = 0.0;
+    let mut z: f64 = 0.0;
+
+    coordinates.iter().for_each(|coordinate| {
+        let (lat, lon) = coordinate.degrees_to_radians();
+
+        x += lat.cos() * lon.cos();
+        y += lat.cos() * lon.sin();
+        z += lat.sin();
+    });
+
+    let num_coords = coordinates.len() as f64;
+
+    x /= num_coords;
+    y /= num_coords;
+    z /= num_coords;
+
+    let longitude = y.atan2(x);
+    let hyp = (x * x + y * y).sqrt();
+    let latitude = z.atan2(hyp);
+
+    (radians_to_degrees(latitude), radians_to_degrees(longitude))
+}
+
+fn radians_to_degrees(radians: f64) -> f64 {
+    radians * 180.0 / PI
+}
--- src/main.rs
+++ src/main.rs
@@ -1,45 +1,13 @@
-use std::f64::consts::PI;
+mod coordinates;
+use coordinates::{coordinates_from_vectors, get_center_from_degrees};
 
-fn main() {
+fn main() -> Result<(), Box<dyn std::error::Error>> {
     let lats: Vec<f64> = vec![33.6956461, 34.1984435];
     let longs: Vec<f64> = vec![-78.8900409, -79.7671658];
-    println!("{:?}", get_center_from_degrees(lats, longs));
-}
+    let coordinates = coordinates_from_vectors(lats, longs)?;
+    let center = get_center_from_degrees(coordinates);
 
-fn radians_to_degrees(radians: f64) -> f64 {
-    radians * 180.0 / PI
-}
+    println!("Center coordinates: ({}, {})", center.0, center.1);
 
-fn degrees_to_radians(degrees: f64) -> f64 {
-    degrees * PI / 180.0
-}
-
-fn get_center_from_degrees(latitudes: Vec<f64>, longitudes: Vec<f64>) -> (f64, f64) {
-    let num_coords = latitudes.len();
-    let mut x: f64 = 0.0;
-    let mut y: f64 = 0.0;
-    let mut z: f64 = 0.0;
-    let mut lat: f64;
-    let mut lon: f64;
-
-    for i in 0..num_coords {
-        lat = degrees_to_radians(latitudes[i]);
-        lon = degrees_to_radians(longitudes[i]);
-
-        x += lat.cos() * lon.cos();
-        y += lat.cos() * lon.sin();
-        z += lat.sin();
-    }
-
-    let num_coords = num_coords as f64;
-
-    x /= num_coords;
-    y /= num_coords;
-    z /= num_coords;
-
-    lon = y.atan2(x);
-    let hyp = (x * x + y * y).sqrt();
-    lat = z.atan2(hyp);
-
-    (radians_to_degrees(lat), radians_to_degrees(lon))
+    Ok(())
 }
